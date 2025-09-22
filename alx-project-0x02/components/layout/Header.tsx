336874a2aff756b97b78fa2eadd9a11ac140d792
Dennis Rua <ruadennis20@gmail.com>
2025-09-22 13:08:34 +0300

    Add basic routing with /home and /about pages and update Header navigation

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="flex space-x-6">
+        <Link href="/" className="hover:text-gray-300">
+          Home
+        </Link>
+        <Link href="/about" className="hover:text-gray-300">
+          About
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
