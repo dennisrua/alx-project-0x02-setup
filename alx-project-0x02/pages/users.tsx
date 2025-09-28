7bc0b843144b5c008159b7cb1b165e56569ba1fa
Dennis Rua <ruadennis20@gmail.com>
2025-09-28 13:03:35 +0300

    feat: add UserCard component and fetch/display users on /users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+// pages/users.tsx
 import UserCard from '@/components/common/UserCard';
 import { UserProps } from '@/interfaces';
 
@@ -8,9 +8,9 @@ interface UsersPageProps {
 
 const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
-    <div className="p-6 space-y-4">
-      <h1 className="text-2xl font-bold mb-4">Users</h1>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Users</h1>
+      <div className="grid gap-4">
         {users.map((user) => (
           <UserCard key={user.name} {...user} />
         ))}
@@ -19,7 +19,9 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export default UsersPage;
+
+export async function getStaticProps() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: UserProps[] = await res.json();
 
@@ -28,6 +30,4 @@ export const getStaticProps: GetStaticProps = async () => {
       users,
     },
   };
-};
-
-export default UsersPage;
+}
